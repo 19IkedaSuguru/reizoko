b10c021118eb38969a9d3d3993dca8647b4243d8
Ikeda Suguru <kareidaisuki000@gmail.com>
2019-10-14 07:39:20 +0000

    10/14

---
+++ database/migrations/2019_10_13_120007_create_food_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFoodTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('food', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id');
+            $table->string('name');
+            $table->date('limit_date');
+            $table->date('purchase_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('food');
+    }
+}
