ec34f11805d1546fe7138f6b114b00fb0ff78555
Ikeda Suguru <kareidaisuki000@gmail.com>
2019-10-17 21:58:06 +0000

    10/18朝まで

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group([], function(){
+    Route::get('food/save', 'FoodController@add');
+    Route::get('food/exist', 'FoodController@add');
+});
