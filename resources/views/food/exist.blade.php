6f5c11597cbe623504314ef6d53eb9b25845fb08
Ikeda Suguru <kareidaisuki000@gmail.com>
2019-10-20 22:20:51 +0000

    11/21朝

--- resources/views/food/exist.blade.php
+++ resources/views/food/exist.blade.php
@@ -5,10 +5,10 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=divice-width, initial-scale=1">
         
-        <title>食材保存画面</title>
+        <title>食材確認画面</title>
     </head>
     <body>
-        <h1>食材保存画面</h1>
-        <P>購入した食材を入力してください</p>
+        <h1>食材確認画面</h1>
+        <P>保存している食材</p>
     </dody>
 </html>
