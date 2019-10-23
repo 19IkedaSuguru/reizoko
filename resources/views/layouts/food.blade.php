acd68a9c8372567b53192a0e912a9aa6ba73db24
Ikeda Suguru <kareidaisuki000@gmail.com>
2019-10-23 22:33:54 +0000

    10/24

--- resources/views/layouts/food.blade.php
+++ resources/views/layouts/food.blade.php
@@ -5,4 +5,30 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=divice-width, initial-scale">
         
+        {{-- CSRF Token --}}
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         
+        {{-- 各ページ毎にtitleを入れる為 --}}
+        <title>@yield('title')</title>
+        
+        {{-- Script --}}
+        <script src="{{ secure_asset('js/app.js') }}" defer></script>
+        
+        {{-- Font --}}
+        <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+        
+        {{-- Styles Laravel標準css--}}
+        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+        {{-- 自作cssの読み込み--}}
+        <link href="{{ secure_asset('css/food.css') }}" rel="stylesheet">>
+    </head>
+    <body>
+        <div id="app">
+            <main class="py-4">
+                {{-- コンテンツを入れる為の＠yield --}}
+                @yield('content')
+            </main>
+        </div>
+    /body>
+</html>
